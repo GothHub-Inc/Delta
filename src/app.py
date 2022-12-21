9612ac514d113e1f26ed26ca5dee2aa0dcf82d98
Alwin <meijboom.am@gmail.com>
2022-12-21 16:13:01 +0100

    Setup base structure

--- src/app.py
+++ src/app.py
@@ -1,6 +1,21 @@
 from discord.ext import commands
 import discord
+import asyncio
+import os
 
-client = commands.Bot(command_prefix="$", intents=discord.Intents.all())
+client = commands.Bot(command_prefix="$", intents=discord.Intents.default())
 
-client.run(token="")
+
+async def load_extensions():
+    for file in os.listdir('./src/extensions'):
+        if file.endswith('.py'):
+            await client.load_extension(f"extensions.{file[:-3]}")
+
+
+async def main():
+    async with client:
+        await load_extensions()
+        await client.start("")
+
+
+asyncio.run(main())
---
+++ src/extensions/user.py
@@ -0,0 +1,10 @@
+from discord.ext import commands
+
+
+class User(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+
+async def setup(bot):
+    await bot.add_cog(User(bot))
