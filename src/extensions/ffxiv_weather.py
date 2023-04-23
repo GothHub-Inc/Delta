fcd277cf3d8fd48d28267d7accaef1b640d64c59
rommert <mailme@rommertstraatsma.nl>
2023-04-23 14:30:19 +0200

    Improve forecaster methods

--- src/extensions/ffxiv_weather.py
+++ src/extensions/ffxiv_weather.py
@@ -4,16 +4,12 @@ import discord
 import datetime
 import ffxivweather
 
-list = ["Limsa Lominsa", "Middle La Noscea", "Lower La Noscea", 
-    "Eastern La Noscea", "Western La Noscea", "Upper La Noscea", 
-    "Outer La Noscea", "Mist"]
-
 
 class FFXIVWeather(commands.Cog):
     def __init__(self, bot):
         self.client = bot
 
-    def __create_embed(self, zone, amount: int = 15):
+    def __create_forecast(self, zone: str, amount: int = 15):
         try:
             forecast = ffxivweather.forecaster.get_forecast(
                 place_name=zone,
@@ -25,18 +21,20 @@ class FFXIVWeather(commands.Cog):
                 description=f"Location {zone} could not be found.",
             )
             return failEmbed
-
-        embed = discord.Embed(
-            title="FFXIV weather forecast",
-            description=f"Forecast of the upcomming weather in {zone}",
-            color=discord.Colour.blue(),
-        )
         weathers = ""
         times = ""
         for weather, start_time in forecast:
             weathers += weather["name_en"]+"\n"
             times += str(
                 datetime.datetime.time(start_time).strftime("%H:%M"))+"\n"
+        return self.__create_embed(zone, weathers, times)
+
+    def __create_embed(self, zone: str, weathers: str, times: str):
+        embed = discord.Embed(
+            title="FFXIV weather forecast",
+            description=f"Forecast of the upcomming weather in {zone}",
+            color=discord.Colour.blue()
+        )
         embed.add_field(
             name="Weather",
             value=weathers,
@@ -56,11 +54,9 @@ class FFXIVWeather(commands.Cog):
     async def ffxivweather(
         self,
         ctx: discord.ApplicationContext,
-        zone: discord.Option(
-                str, autocomplete=discord.utils.basic_autocomplete(list)
-            )
+        zone: str
     ):
-        await ctx.respond(embed=self.__create_embed(zone, 5))
+        await ctx.respond(embed=self.__create_forecast(zone, 5))
 
 
 def setup(bot):
